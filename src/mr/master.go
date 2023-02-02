544c691d8cbddf137297cf55eaaafb1195255c08
AlexYoungZ <zhang.siyan@northeastern.edu>
2023-02-02 11:30:00 -0800

    add master & worker

--- src/mr/master.go
+++ src/mr/master.go
@@ -41,16 +41,125 @@ type Master struct {
 	taskCh    chan Task
 }
 
-func (m *Master) getTask(taskSeq int) Task           {}
-func (m *Master) schedule()                          {}
-func (m *Master) initMapTask()                       {}
-func (m *Master) initReduceTask()                    {}
-func (m *Master) regTask(args *TaskArgs, task *Task) {}
+func (m *Master) getTask(taskSeq int) Task {
+	task := Task{
+		FileName: "",
+		NReduce:  m.nReduce,
+		NMaps:    len(m.files),
+		Seq:      taskSeq,
+		Phase:    m.taskPhase,
+		Alive:    true,
+	}
+	DPrintf("m:%+v, taskseq:%d, lenfiles:%d, lents:%d", m, taskSeq, len(m.files), len(m.taskStats))
+	if task.Phase == MapPhase {
+		task.FileName = m.files[taskSeq]
+	}
+	return task
+}
+
+func (m *Master) schedule() {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if m.done {
+		return
+	}
+	allFinish := true
+	for index, t := range m.taskStats {
+		switch t.Status {
+		case TaskStatusReady:
+			allFinish = false
+			m.taskCh <- m.getTask(index)
+			m.taskStats[index].Status = TaskStatusQueue
+		case TaskStatusQueue:
+			allFinish = false
+		case TaskStatusRunning:
+			allFinish = false
+			if time.Now().Sub(t.StartTime) > MaxTaskRunTime {
+				m.taskStats[index].Status = TaskStatusQueue
+				m.taskCh <- m.getTask(index)
+			}
+		case TaskStatusFinish:
+		case TaskStatusErr:
+			allFinish = false
+			m.taskStats[index].Status = TaskStatusQueue
+			m.taskCh <- m.getTask(index)
+		default:
+			panic("t.status err")
+		}
+	}
+	if allFinish {
+		if m.taskPhase == MapPhase {
+			m.initReduceTask()
+		} else {
+			m.done = true
+		}
+	}
+}
+
+func (m *Master) initMapTask() {
+	m.taskPhase = MapPhase
+	m.taskStats = make([]TaskStat, len(m.files))
+}
+
+func (m *Master) initReduceTask() {
+	DPrintf("init ReduceTask")
+	m.taskPhase = ReducePhase
+	m.taskStats = make([]TaskStat, m.nReduce)
+}
+
+func (m *Master) regTask(args *TaskArgs, task *Task) {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	if task.Phase != m.taskPhase {
+		panic("req Task phase neq")
+	}
+
+	m.taskStats[task.Seq].Status = TaskStatusRunning
+	m.taskStats[task.Seq].WorkerId = args.WorkerId
+	m.taskStats[task.Seq].StartTime = time.Now()
+}
 
 // TODO Your code here -- RPC handlers for the worker to call.
-func (m *Master) GetOneTask(args *TaskArgs, reply *TaskReply) error             {}
-func (m *Master) ReportTask(args *ReportTaskArgs, reply *ReportTaskReply) error {}
-func (m *Master) RegWorker(args *RegisterArgs, reply *RegisterReply) error      {}
+func (m *Master) GetOneTask(args *TaskArgs, reply *TaskReply) error {
+	task := <-m.taskCh
+	reply.Task = &task
+
+	if task.Alive {
+		m.regTask(args, &task)
+	}
+	DPrintf("in get one Task, args:%+v, reply:%+v", args, reply)
+	return nil
+}
+
+func (m *Master) ReportTask(args *ReportTaskArgs, reply *ReportTaskReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+
+	DPrintf("get report task: %+v, taskPhase: %+v", args, m.taskPhase)
+
+	if m.taskPhase != args.Phase || args.WorkerId != m.taskStats[args.Seq].WorkerId {
+		return nil
+	}
+
+	if args.Done {
+		m.taskStats[args.Seq].Status = TaskStatusFinish
+	} else {
+		m.taskStats[args.Seq].Status = TaskStatusErr
+	}
+
+	go m.schedule()
+	return nil
+}
+
+func (m *Master) RegWorker(args *RegisterArgs, reply *RegisterReply) error {
+	m.mu.Lock()
+	defer m.mu.Unlock()
+	m.workerSeq += 1
+	reply.WorkerId = m.workerSeq
+	return nil
+}
 
 // start a thread that listens for RPCs from worker.go
 func (m *Master) server() {
@@ -93,8 +202,19 @@ func MakeMaster(files []string, nReduce int) *Master {
 	m := Master{}
 
 	// TODO Your code here.
+	m.mu = sync.Mutex{}
+	m.nReduce = nReduce
+	m.files = files
+	if nReduce > len(files) {
+		m.taskCh = make(chan Task, nReduce)
+	} else {
+		m.taskCh = make(chan Task, len(m.files))
+	}
 
+	m.initMapTask()
+	go m.tickSchedule()
 	m.server()
+	DPrintf("master init")
 	return &m
 }
 
--- src/mr/worker.go
+++ src/mr/worker.go
@@ -1,6 +1,12 @@
 package mr
 
-import "fmt"
+import (
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"strings"
+)
 import "log"
 import "net/rpc"
 import "hash/fnv"
@@ -25,7 +31,11 @@ func Worker(mapf func(string, string) []KeyValue,
 	reducef func(string, []string) string) {
 
 	// TODO Your worker implementation here.
-
+	w := worker{}
+	w.mapf = mapf
+	w.reducef = reducef
+	w.register()
+	w.run()
 	// uncomment to send the Example RPC to the master.
 	// CallExample()
 
@@ -38,31 +48,134 @@ type worker struct {
 }
 
 func (w *worker) run() {
-
+	// if reqTask conn fail, worker exit
+	for {
+		t := w.reqTask()
+		if !t.Alive {
+			DPrintf("worker get task not alive, exit")
+			return
+		}
+		w.doTask(t)
+	}
 }
 
 func (w *worker) reqTask() Task {
+	args := TaskArgs{}
+	args.WorkerId = w.id
+	reply := TaskReply{}
 
+	if ok := call("Master.GetOneTask", &args, &reply); !ok {
+		DPrintf("worker get task fail,exit")
+		os.Exit(1)
+	}
+	DPrintf("worker get task:%+v", reply.Task)
+	return *reply.Task
 }
 
 func (w *worker) doTask(t Task) {
+	DPrintf("in do Task")
 
+	switch t.Phase {
+	case MapPhase:
+		w.doMapTask(t)
+	case ReducePhase:
+		w.doReduceTask(t)
+	default:
+		panic(fmt.Sprintf("task phase err: %v", t.Phase))
+	}
 }
 
 func (w *worker) doMapTask(t Task) {
+	contents, err := ioutil.ReadFile(t.FileName)
+	if err != nil {
+		w.reportTask(t, false, err)
+		return
+	}
 
+	kvs := w.mapf(t.FileName, string(contents))
+	reduces := make([][]KeyValue, t.NReduce)
+	for _, kv := range kvs {
+		idx := ihash(kv.Key) % t.NReduce
+		reduces[idx] = append(reduces[idx], kv)
+	}
+
+	for idx, l := range reduces {
+		fileName := reduceName(t.Seq, idx)
+		f, err := os.Create(fileName)
+		if err != nil {
+			w.reportTask(t, false, err)
+			return
+		}
+		enc := json.NewEncoder(f)
+		for _, kv := range l {
+			if err := enc.Encode(&kv); err != nil {
+				w.reportTask(t, false, err)
+			}
+
+		}
+		if err := f.Close(); err != nil {
+			w.reportTask(t, false, err)
+		}
+	}
+	w.reportTask(t, true, nil)
 }
 
 func (w *worker) doReduceTask(t Task) {
+	maps := make(map[string][]string)
+	for idx := 0; idx < t.NMaps; idx++ {
+		fileName := reduceName(idx, t.Seq)
+		file, err := os.Open(fileName)
+		if err != nil {
+			w.reportTask(t, false, err)
+			return
+		}
+		dec := json.NewDecoder(file)
+		for {
+			var kv KeyValue
+			if err := dec.Decode(&kv); err != nil {
+				break
+			}
+			if _, ok := maps[kv.Key]; !ok {
+				maps[kv.Key] = make([]string, 0, 100)
+			}
+			maps[kv.Key] = append(maps[kv.Key], kv.Value)
+		}
+	}
 
+	res := make([]string, 0, 100)
+	for k, v := range maps {
+		res = append(res, fmt.Sprintf("%v %v\n", k, w.reducef(k, v)))
+	}
+
+	if err := ioutil.WriteFile(mergeName(t.Seq), []byte(strings.Join(res, "")), 0600); err != nil {
+		w.reportTask(t, false, err)
+	}
+
+	w.reportTask(t, true, nil)
 }
 
 func (w *worker) reportTask(t Task, done bool, err error) {
-
+	if err != nil {
+		log.Printf("%v", err)
+	}
+	args := ReportTaskArgs{}
+	args.Done = done
+	args.Seq = t.Seq
+	args.Phase = t.Phase
+	args.WorkerId = w.id
+	reply := ReportTaskReply{}
+	if ok := call("Master.ReportTask", &args, &reply); !ok {
+		DPrintf("report task fail:%+v", args)
+	}
 }
 
 func (w *worker) register() {
-
+	args := &RegisterArgs{}
+	reply := &RegisterReply{}
+	if ok := call("Master.RegWorker", args, reply); !ok {
+		log.Fatal("reg fail")
+	}
+	w.id = reply.WorkerId
 }
 
 // send an RPC request to the master, wait for the response.
